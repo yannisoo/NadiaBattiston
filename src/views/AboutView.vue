2b6e3eadda567f5d71d2fa38a88a6d1ab366f7fa
Yannis Battiston <yannis@ymoney.com>
2023-01-01 18:44:15 +0100

    about changes and citroen c5 link

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,62 +1,82 @@
 <script setup lang="ts"></script>
 <template>
+  <h1>Qui-suis-je?</h1>
   <section>
-    <h2>Qui suis-je</h2>
-    <p>
-      Je suis née à Paris de parents algériens dont je parle le dialecte avec aisance.
-    </p>
-    <p>
-      A 7 ans, j’ai déjà une bonne élocution. J’aime lire à haute voix pour faire vivre les personnages. Quelque chose
-      frissonne en moi…
-    </p>
-    <p>
-      10 ans ! Je lis ma première pièce de Shakespeare.
-      Je suis subjuguée par sa plume, par ses mots mais je ne comprends rien !
-    </p>
-    <p>
-      Et depuis, je voue une véritable passion pour la comédie, la poésie, le théâtre…
-      J’apprends encore et toujours des textes qui résonnent en moi.
-    </p>
-  </section>
-  <section>
-    <h2>Mes expériences</h2>
-    <p>
-      Comédienne depuis 16 ans je me spécialise dans le doublage, voice over, narration, radio, films institutionnels et
-      publicités.
-    </p>
-    <p>
+    <img src="/images/4web.jpg" alt="">
 
-      Ma voix peut être douce et enveloppante ou grave et vibrante. J’adapte mon travail en fonction de la demande du
-      client le plus exigeant !
-    </p>
-    <p> Alors écoutez moi et dites moi.
-    </p>
-  </section>
-  <section>
-    <h2>Comment je travaille</h2>
-    <p>J’enregistre la plupart du temps dans des studios professionels.</p>
-    <p>
-      Pour les enregistrements à distance je suis équipée d'un Neumann u 87 ai studio set.</p>
-    <p>
-      Pour une demande de tarif merci de bien vouloir me consulter.
-    </p>
-  </section>
-  <section>
-    <h2>Contact</h2>
-    <p>
-      Nadia Battiston
-    </p>
-    <p>
-      n.battiston@outlook.fr
-    </p>
-    <p>
-      06.12.79.13.07
-    </p>
+    <div class="text">
+
+      <p>
+        Je suis née à Paris de parents algériens dont je parle le dialecte avec aisance.
+        <br>
+        À 7 ans, j'ai déjà une bonne élocution. J'aime lire à haute voix pour faire vivre les personnages. Quelque chose
+        frissonne en moi...
+        <br>
+        10 ans ! Je lis ma première pièce de Shakespeare. Je suis subjuguée par sa plume, par ces mots, mais je ne
+        comprends rien !
+      </p>
+      <br>
+      <p>
+        En 1987, j'obtiens un BAC littéraire option Art Dramatique. J'intègre ensuite, pendant trois ans, l'École de
+        Théâtre T.E.M Montreuil, sous la direction de Michel BLIN. Nous avons travaillé sur différents auteurs, et
+        monter
+        avec notre troupe la pièce de théâtre "La Poche Parmentier" de Georges Perec.
+      </p>
+      <br>
+      <p>
+        En parallèle, je travaille chez "Première Heure". C'est là que je fais mes premiers pas dans la voix off. J'ai
+        cette facilité à placer et à poser ma voix.
+        <br>
+        J'aime cette ambiance de travail. C'est une révélation !
+        <br>
+      </p>
+      <br>
+      <p>
+        Je prends mon envol et me spécialise dans le doublage, voice-over, narration, radio, films institutionnels et
+        publicités.
+        <br>
+        Comédienne depuis 20 ans, je voue une véritable passion pour la comédie, la poésie, le théâtre…
+        <br>
+        J’apprends encore et toujours des textes qui résonnent en moi.
+        <br>
+
+      </p>
+    </div>
+
+
+
+
   </section>
 </template>
 
 <style scoped>
+h1{
+  color:white;
+  font-size: 2.65rem;
+  font-family: 'Inter', sans-serif;
+  padding: 2rem 0 0 2rem;
+}
 section {
   color: white;
+  font-family: 'Inter', sans-serif;
+  font-size: 1.2rem;
+  display: flex;
+  padding: 2rem;
+}
+
+@media screen and (max-width: 940px) {
+  section {
+    flex-direction: column;
+  }
+  
+}
+img {
+  width: 30vw;
+}
+
+.text {
+  padding: 2rem;
 }
 </style>
+
+
